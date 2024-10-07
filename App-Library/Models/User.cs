bee8209e4acd15b60cd60c4f85c17f7bcff331a9
KhoiLee <khoile139004@gmail.com>
2024-10-07 22:13:30 +0700

    Project Initiation

---
+++ App-Library/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Library.Models
+{
+    internal class User
+    {
+    }
+}
